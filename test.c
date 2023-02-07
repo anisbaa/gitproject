ec560d37228e5191e8d5b30c83bcad3bc204fad0
anis baazoug <anis.baazoug2000@gmail.com>
2023-02-07 02:19:52 -0800

    modification de fichier test.c

--- test.c
+++ test.c
@@ -3,3 +3,66 @@
 {
       printf("hello diva-software");
 }
+
+<!DOCTYPE html>
+<html>
+   <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+         <title>Mon Premier Site Web avec Chahrazed |  Accueil</title>
+  </head>
+  <body>
+        <header>Menu</header>
+        <main>Section principale</main>
+        <footer>Bas de page</footer>
+  </body>
+</html>
+
+
+<header>
+     <h1>Mon Site Web</h1>
+     <nav>
+        <ul>
+          <li>Accueil</li>
+          <li>A propos</li>
+          <li>Services</li>
+          <li>Contact</li>
+       </ul>
+    </nav>
+</header>
+
+<main>
+      <section id="banner"></section>
+      <section id="features" ></section>
+      <section id="newsletter" ></section>
+</main>
+
+
+<section id="banner">
+    <h1>
+       Mon Premier Site Web
+    </h1>
+    <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Etiam eu luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem interdum condimentum.
+    </p>
+</section>
+<section id="features" >
+    <div>
+        <img src="./img/logo_html.png" />
+        <h3>HTML5</h3>  
+       <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+       </p>
+    </div>
+    <div>
+       <img src="./img/logo_css.png" />
+       <h3>CSS3</h3>
+       <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+       </p>
+    </div>
+    <div>
+        <img src="./img/logo_js.png" />
+        <h3>JS</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies
+        </p>
+     </div>
+</section>
